39910784c0536c55689c0bdd978ef89f81a4bc4f
JianxiongCui <36972316+JianxiongCui@users.noreply.github.com>
2020-09-28 15:35:00 -0400

    add fractional order

--- robin_live_trading/rb_strats.py
+++ robin_live_trading/rb_strats.py
@@ -21,6 +21,7 @@ class robin_strategy():
             'dollar_amt':300,
         }
         self.data = robin_data()
+        self.day_trade_list = {}
 
     def set_param(self,param):
         self.param = param
@@ -31,6 +32,7 @@ class robin_strategy():
         if cash < 0： try dollar_amt order
         if cash > 0:  try dollar_amt_order
         '''
+
         share_quantity_1 = int(dollar_amt/df['close_price'][0]) # if cash_available >= dollar_amt else int(cash_available/df['close_price'][0])
         order_try_1 = rs.orders.order_buy_market(symbol = inputSymbol,quantity = share_quantity_1)
         if 'id' not in order_try_1.keys(): # order failed
@@ -47,13 +49,53 @@ class robin_strategy():
             print('market buy ',share_quantity_1, inputSymbol)
         return 0
 
+    def market_buy_stock_fractional(self,inputSymbol,dollar_amt):
+        '''
+        if cash < 0： try dollar_amt order
+        if cash > 0:  try dollar_amt_order
+        '''
+        
+        # share_quantity_1 = int(dollar_amt/df['close_price'][0]) # if cash_available >= dollar_amt else int(cash_available/df['close_price'][0])
+        order_try_1 = rs.orders.order_buy_fractional_by_price(symbol = inputSymbol,amountInDollars = dollar_amt)
+        if 'id' not in order_try_1.keys(): # order failed
+            # share_quantity_2 = int(order_try_1['detail'].split()[4])
+            # if share_quantity_2 < share_quantity_1: # buy fewer shares
+            #     order_try_2 = rs.orders.order_buy_market(symbol = inputSymbol,quantity = share_quantity_2)
+            #     if 'id' in order_try_2.keys():
+            #         print('market buy ',share_quantity_2, inputSymbol)
+            #     else:
+            #         print('fail to market buy ',inputSymbol)
+            # else:
+            print('fail to market buy ',inputSymbol)
+        else:
+            print('market buy ',order_try_1.keys()['quantity'], inputSymbol)
+            self.day_trade_list.update({inputSymbol:dollar_amt})
+        return 0
+
 
     def market_sell_stock(self,inputSymbol):
-
+        if inputSymbol in self.day_trade_list.keys():
+            print('daytrade sell prevented: ',inputSymbol)
+            return -1
         ## submit market sell
         holdings = rs.build_holdings()[inputSymbol]
         share_quantity = int(float(holdings['quantity']))
         rs.orders.order_sell_market(symbol = inputSymbol,quantity = share_quantity)
+        ## TODO:deal with day-trade error: just leave it failed
+        print('market sell ',share_quantity, inputSymbol)
+
+        return 0
+
+
+    def market_sell_stock_fractional(self,inputSymbol):
+
+        if inputSymbol in self.day_trade_list.keys():
+            print('daytrade sell prevented: ',inputSymbol)
+            return -1
+        ## submit market sell
+        holdings = rs.build_holdings()[inputSymbol]
+        share_quantity = float(holdings['quantity'])
+        rs.orders.order_sell_fractional_by_quantity(symbol = inputSymbol,quantity = share_quantity)
         ## TODO:deal with day-trade error: just leave it failed
         print('market sell ',share_quantity, inputSymbol)
 
@@ -87,7 +129,8 @@ class robin_strategy():
                     ## submit market buy
                     print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                     print('buying signal', inputSymbol)
-                    self.market_buy_stock(inputSymbol,df,dollar_amt)
+                    # self.market_buy_stock(inputSymbol,df,dollar_amt)
+                    self.market_buy_stock_fractional(inputSymbol,dollar_amt)
 
                 else: 
                     print('buying signal, but ',inputSymbol,' position still exists, just hold, no more buying.')
@@ -104,7 +147,7 @@ class robin_strategy():
                     ## submit market sell
                     print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                     print('selling signal', inputSymbol)
-                    self.market_sell_stock(inputSymbol)
+                    self.market_sell_stock_fractional(inputSymbol)
                 else: 
                     print('selling signal, but no ',inputSymbol,' position, just hold, no more selling') 
     
@@ -147,7 +190,8 @@ class robin_strategy():
                         ## submit market buy
                         print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                         print('buying signal', inputSymbol)
-                        self.market_buy_stock(inputSymbol,df,dollar_amt)
+                        # self.market_buy_stock(inputSymbol,df,dollar_amt)
+                        self.market_buy_stock_fractional(inputSymbol,dollar_amt)
                     else: 
                         print('buying signal, but ',inputSymbol,' position still exists, just hold, no more buying.')
 
@@ -158,7 +202,7 @@ class robin_strategy():
                         ## submit market sell
                         print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                         print('selling signal', inputSymbol)
-                        self.market_sell_stock(inputSymbol)
+                        self.market_sell_stock_fractional(inputSymbol)
                     else: 
                         print('selling signal, but no ',inputSymbol,' position, just hold, no more selling') 
             else:    
@@ -267,14 +311,14 @@ class robin_strategy():
         long_window = self.param['long_window']
         signal_window = self.param['signal_window']
         try:
-            df = self.get_stock_historicals_df(inputSymbol,span = 'month')
+            df = self.data.get_stock_historicals_df(inputSymbol,span = 'month')
         except:
             print("stock not found: try fetching as cryptocurrency:")
             try:
-                df = self.get_crypto_historicals_df(inputSymbol,interval = 'day',span = 'month',bound='24_7')
+                df = self.data.get_crypto_historicals_df(inputSymbol,interval = 'day',span = 'month',bound='24_7')
             except:
                 ValueError("cryptocurrency not found")
-        print("data fetched")    
+        # print("data fetched")    
 
         df['EMA_fast'] = df['close_price'].ewm(span = short_window).mean()
         df['EMA_slow'] = df['close_price'].ewm(span = long_window).mean() 
